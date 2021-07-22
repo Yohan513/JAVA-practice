e0554f0b05786ce2450b6ba317a15776151c409a
Park Seungbeom <81613308+SiNaMoNu@users.noreply.github.com>
2021-07-23 04:52:10 +0900

    Create Seungbeom.java

---
+++ 5_일차원 배열/1_최소, 최대/Seungbeom.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] array = new int[N];
+        
+        for(int i=0; i<N; i++) {
+        	array[i] = sc.nextInt();
+        }
+        int max = array[0];
+        int min = array[0];
+        
+        for(int i=0; i<N; i++) {
+        	if(array[i] > max) {
+        		max = array[i];
+        	}
+        }
+        for(int i=0; i<N; i++) {
+        	if(array[i] < min) {
+        		min = array[i];
+        	}
+        }
+        System.out.println(min + " " + max);
+    }
+}
